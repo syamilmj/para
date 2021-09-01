bf610f173c917c4204683cf5e699a659f03ef465
Syamil MJ <sy4mil@gmail.com>
2021-09-02 02:13:52 +0800

    Fill up project metadata

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule Para.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      description: "A toolkit for data mapping and language integrated query for Elixir",
+      package: package(),
+
+      # Docs
+      name: "Ecto",
+      docs: docs()
     ]
   end
 
@@ -21,5 +29,20 @@ defmodule Para.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [{:ecto, "~> 3.7"}]
+  end
+
+  def package do
+    [
+      files: ~w(lib mix.exs README* LICENSE),
+      maintainers: ["Syamil MJ"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/syamilmj/para"}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: "README.md"
+    ]
   end
 end
